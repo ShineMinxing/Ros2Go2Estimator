84c033b4dd941aa40eff6ae586392217929a441b
ShineMinxing <401435318@qq.com>
2025-05-22 19:56:46 +0800

    stop auto start rviz

--- sport_control/launch/go2_launch.py
+++ sport_control/launch/go2_launch.py
@@ -59,24 +59,24 @@ def generate_launch_description():
             ]
         ),
 
-        TimerAction(
-            period=3.0,
-            actions=[
-                ExecuteProcess(
-                    cmd=[
-                        "x-terminal-emulator",
-                        "--new-process",
-                        "-e",
-                        'bash',
-                        '-c',
-                        'source ~/.bashrc && '
-                        'rviz2 -d ~/ros2_ws/LeggedRobot/src/Ros2Go2Estimator/other/SMXFE_odm.rviz;'
-                        'read -p "Press enter to close"'
-                    ],
-                    output='screen',
-                )
-            ]
-        ),
+        # TimerAction(
+        #     period=3.0,
+        #     actions=[
+        #         ExecuteProcess(
+        #             cmd=[
+        #                 "x-terminal-emulator",
+        #                 "--new-process",
+        #                 "-e",
+        #                 'bash',
+        #                 '-c',
+        #                 'source ~/.bashrc && '
+        #                 'rviz2 -d ~/ros2_ws/LeggedRobot/src/Ros2Go2Estimator/other/SMXFE_odm.rviz;'
+        #                 'read -p "Press enter to close"'
+        #             ],
+        #             output='screen',
+        #         )
+        #     ]
+        # ),
     ]
 )
 
